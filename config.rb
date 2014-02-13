9a612471b404f340b706e9a2c2678f3d9fbcae44
Gregor Wassmann <gregor.wassmann@gmail.com>
2014-02-13 23:34:00 +0100

    Refactor to multiple files

--- config.rb
+++ config.rb
@@ -9,11 +9,15 @@ activate :livereload
 require 'tumblargh'
 activate :tumblargh,
   api_key: 'i41pISnf18m39MT1kMQLsRPC8mU7XcOem6eYALzkLcCoKgTtgm', # This is your OAuth consumer key
-  blog: 'kunsttag.tumblr.com.json'
+  blog: 'kunsttag.tumblr.com'
 
 page "/channel.html", layout: false
 
 I18n.enforce_available_locales = false
+
+# set :css_dir, 'assets/stylesheets'
+# set :js_dir, 'assets/javascripts'
+# set :images_dir, 'assets/images'
 
 # Build-specific configuration
 configure :build do
@@ -25,6 +29,9 @@ configure :build do
   # Setting CSS dir does not work in build
   # set :css_dir, '//44ft.com/kunsttag_assets'
   
+  # Or use a different asset path
+  set :http_prefix, "//44ft.com/kunsttag_assets/"  
+  
   # For example, change the Compass output style for deployment
   activate :minify_css
   
@@ -33,7 +40,4 @@ configure :build do
   
   # Enable cache buster
   activate :asset_hash, exts: ['.js', '.css'], ignore: ['modernizr-2.6.2.min.js']
-  
-  # Or use a different asset path
-  set :http_prefix, "//44ft.com/kunsttag_assets/"
 end
